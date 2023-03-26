264750f976ba33cb27192fc901439386e36eb339
Deren Somsanith <derensomsanith@gmail.com>
2023-03-26 17:34:38 -0500

    stupid thing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,37 @@
-"use client";
+// "use client";
 
-import "~/styles/globals.css";
+// import "~/styles/globals.css";
 
-import { getServerSession } from "next-auth";
-import SessionWrapper from "~/components/SessionWrapper";
-import Sidebar from "~/components/Sidebar";
-import { authOptions } from "~/server/auth";
-import TrpcWrapper from "~/components/TrpcWrapper";
+// import { getServerSession } from "next-auth";
+// import SessionWrapper from "~/components/SessionWrapper";
+// import Sidebar from "~/components/Sidebar";
+// import { authOptions } from "~/server/auth";
+// import TrpcWrapper from "~/components/TrpcWrapper";
 
-export const metadata = {
-  title: "Deren - ChatGPT",
-  description: "Deren ChatGPT",
-};
+// export const metadata = {
+//   title: "Deren - ChatGPT",
+//   description: "Deren ChatGPT",
+// };
 
-export async function RootLayout({ children }: { children: React.ReactNode }) {
-  const session = await getServerSession(authOptions);
+// export async function RootLayout({ children }: { children: React.ReactNode }) {
+//   const session = await getServerSession(authOptions);
 
-  return (
-    <html lang="en">
-      <body>
-        <TrpcWrapper>
-          <SessionWrapper session={session}>
-            <div className="flex">
-              <div className="h-screen max-w-xs overflow-y-auto bg-gray-900 md:min-w-[20rem]">
-                <Sidebar />
-              </div>
-              <div className="flex-1 bg-gray-800">{children}</div>
-            </div>
-          </SessionWrapper>
-        </TrpcWrapper>
-      </body>
-    </html>
-  );
-}
+//   return (
+//     <html lang="en">
+//       <body>
+//         <TrpcWrapper>
+//           <SessionWrapper session={session}>
+//             <div className="flex">
+//               <div className="h-screen max-w-xs overflow-y-auto bg-gray-900 md:min-w-[20rem]">
+//                 <Sidebar />
+//               </div>
+//               <div className="flex-1 bg-gray-800">{children}</div>
+//             </div>
+//           </SessionWrapper>
+//         </TrpcWrapper>
+//       </body>
+//     </html>
+//   );
+// }
 
-export default RootLayout;
+// export default RootLayout;
